d48388e540917c422bb63bd02dd780a1fd4900d8
mayonnaises <ernani.184404@gmail.com>
2021-08-22 09:54:28 +0900

    Update Vue

---
+++ src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="search_block">
+    <form id="search_form">
+      <input type="text" id="search_input" />
+      <button type="submit" id="search_submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+#search_input {
+  padding: 4px 6px;
+  font-size: 15px;
+  font-weight: 600;
+  color: #333;
+}
+#search_submit {
+  padding: 4px 3px;
+  font-size: 15px;
+  color: #333;
+  border-radius: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search msg="Hello World"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Search from '@/components/Search.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Search
   }
 }
 </script>
